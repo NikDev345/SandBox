804b5c0a018ae2bb4413c663e74067b42619445f
Nikhil <nikhilrangarej@gmail.com>
2026-08-10 15:54:02 +0530

    fix Vercel API route

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ fast_app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-@fast_app.get("/")
+@fast_app.get("/api")
 def root():
     return {"message": "SandBox API is running"}
 # tools
