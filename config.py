587a08c15a29daae7ea76ed0545d0854bc2dafab
Nikhil <nikhilrangarej@gmail.com>
2026-08-11 14:49:34 +0530

    added

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+APP_BASE_URL = os.getenv("APP_BASE_URL")
 email_api_key = os.getenv('EMAIL_API')
 email_from = os.getenv('EMAIL_FROM')
 GEMINI_MODEL = os.getenv("GEMINI_MODEL")
